661f03dae16f6b9acc93e2909049c611fd4f0d23
Uceey <ucoma12@gmail.com>
2024-12-23 15:36:48 +0100

    first k8s provision

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "google" {
+  project     = "my-k8s-project-445604"
+  region      = "us-central1"
+}
+
+resource "google_container_cluster" "k8s_cluster" {
+  name     = "primary-k8s-cluster"
+  location = "us-central1"
+  initial_node_count = 3
+  node_config {
+    machine_type = "e2-standard-2"
+  }
+}
+
+output "kubeconfig" {
+  value = google_container_cluster.k8s_cluster.endpoint
+}
+terraform { 
+  cloud { 
+    
+    organization = "silverink" 
+
+    workspaces { 
+      name = "kube-infra" 
+    } 
+  } 
+}
